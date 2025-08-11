079e8e1d94cea0a396fd1ca747cb869a3ca30111
Galushko Evgeniy <yudginchik@gmail.com>
2025-08-11 21:05:24 +0300

    start

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -4,7 +4,7 @@ import s from "./Logo.module.css";
 export default function Logo() {
   return (
     <NavLink to="/" className={s.logo}>
-      <img src="/public/logo.svg" alt="logo" />
+      <img src="/public/logo.png" alt="logo" />
       Трекер Глюкозы
     </NavLink>
   );
